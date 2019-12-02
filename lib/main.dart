300d92ea3df2443e2d9b39b3ba2ae0cfdf4f4e4b
newaz <newazredoy99@gmail.com>
2019-12-02 16:37:43 +0600

    color change.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,12 @@ void main() => runApp(MyApp());
 
 
 class MyApp extends StatelessWidget {
+  hexColor (String colorCode) {
+    String colorNew= '0xff' + colorCode;
+    colorNew= colorNew.replaceAll('#', '');
+    int colorInt = int.parse(colorNew);
+    return colorInt;
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
