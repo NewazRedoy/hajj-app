5030f2e4c60284611ae9e8ea76f2656d0bba0603
newaz <newazredoy99@gmail.com>
2019-12-06 22:04:58 +0600

    trying database helper.

--- lib/model/database_helper.dart
+++ lib/model/database_helper.dart
@@ -1,2 +1,13 @@
 import 'dart:io';
 import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:first_app/model/GridList.dart';
+// singleton class to manage the database
+class DatabaseHelper {
+  // This is the actual database filename that is saved in the docs directory.
+  static final _databaseName = "hajj.db";
+
+  // Increment this version when you need to change the schema.
+  static final _databaseVersion = 1;
+}
