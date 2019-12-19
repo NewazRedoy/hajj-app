97b905a9bb5c3ea8bb413257632a6ec3f9f6c22b
newaz <newazredoy99@gmail.com>
2019-12-19 12:51:41 +0600

    error solve in dua page

--- lib/screen/DuaPage.dart
+++ lib/screen/DuaPage.dart
@@ -3,22 +3,11 @@ import 'package:first_app/util/Constants.dart';
 import 'package:first_app/model/DuaTopic.dart';
 
 class DuaPage extends StatefulWidget {
-  final DuaTopic dua_categories;
-
-
-  const DuaPage({Key key, @required this.dua_categories,})
-      : super(key: key);
-
   @override
-  _DuaPageState createState() =>
-      _DuaPageState(dua_categories);
+  _DuaPageState createState() => _DuaPageState();
 }
 
 class _DuaPageState extends State<DuaPage> {
-  DuaTopic dua_categories;
-
-  _DuaPageState(this.dua_categories);
-
   List data = [];
   var loading = true;
 
@@ -37,75 +26,79 @@ class _DuaPageState extends State<DuaPage> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          dua_categories.duatopic,
-        ),
+        title: Text("নির্বাচিত প্রশ্নোত্তর"),
       ),
-      body: ListView.builder(
+      body: loading
+          ? _buildCircularProgressIndicator()
+          : ListView.builder(
         itemCount: data.length,
         itemBuilder: (context, index) {
-          return NewWidget(data[index]);
+          return NewWidget(index, data[index]);
         },
       ),
     );
   }
 }
 
-
-
-
-
+_buildCircularProgressIndicator() {
+  return Center(
+    child: CircularProgressIndicator(),
+  );
+}
 
 class NewWidget extends StatelessWidget {
-  DuaTopic duaTopic;
   int index;
-  NewWidget(this.duaTopic,this.index);
+  DuaTopic duaTopic;
 
-@override
+  NewWidget(this.index, this.duaTopic);
+
+  @override
   Widget build(BuildContext context) {
-  return Card(
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      elevation: 7.0,
-      child: InkWell(
-        onTap: () {},
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(color: Colors.amber),
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  index.toString(),
-                  style: TextStyle(color: Colors.white, fontSize: 20),
+    return Card(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        elevation: 7.0,
+        child: InkWell(
+          onTap: () {
+
+          },
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(color: Colors.amber),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    index.toString(),
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 8,
-              ),
-              Expanded(
-                child: Text(
-                  duaTopic.duatopic,
-                  style: TextStyle(fontSize: 20),
+                SizedBox(
+                  width: 8,
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.amber),
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "",
-                  style: TextStyle(fontSize: 20),
+                Expanded(
+                  child: Text(
+                    duaTopic.duatopic,
+                    style: TextStyle(fontSize: 20),
+                  ),
                 ),
-              )
-            ]),
-      ));
+                Container(
+                  decoration: BoxDecoration(color: Colors.amber),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ]),
+        ));
+  }
 }
 
 
