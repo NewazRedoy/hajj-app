98caa5ab5899a0d854cf2efe80549b684f942e3f
newaz <newazredoy99@gmail.com>
2019-12-25 01:56:35 +0600

    changes

--- lib/screen/questions/SendQuestionPage.dart
+++ lib/screen/questions/SendQuestionPage.dart
@@ -12,62 +12,63 @@ class _SendQuestionPageState extends State<SendQuestionPage> {
       appBar: AppBar(
         title: Text("প্রশ্ন করুন"),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(12),
-            child: Card(
-              elevation: 10.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(2.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                          labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                          labelText: 'Title',
-                          hintText: "hint"),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                          labelText: 'Description',
-                          hintText: "hint"),
-                    ),
-                  ],
+      body:  Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(12),
+              child: Card(
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                            hintText: "প্রশ্নের বিষয় উল্লেখ করুন (যেমনঃ ইহরাম, তাওয়াফ, সা'ঈ)"),
+                      ),
+                      TextField(
+                        maxLines: 4,
+                        decoration: InputDecoration(
+                            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                            hintText: "আপনার দু'আ এখানে লেখুন..."),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 140.0,
-          ),
-          Center(
-            child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: InkWell(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: Column(
-                      children: <Widget>[
-                        Icon(Icons.send),
-                        SizedBox(
-                          height: 12,
-                        ),
-                        Text("পাঠিয়ে দিন")
-                      ],
-                    ),
+            SizedBox(
+              height: 140.0,
+            ),
+            Center(
+              child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
-                )),
-          ),
-        ],
-      ),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
+                        children: <Widget>[
+                          RotatedBox( quarterTurns: 3,
+                              child: Icon(Icons.send)),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text("পাঠিয়ে দিন")
+                        ],
+                      ),
+                    ),
+                  )),
+            ),
+          ],
+        ),
+
     );
   }
 }
