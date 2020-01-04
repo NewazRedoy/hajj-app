a56222e0d38f239ee2882f75b33a9d0be40bc992
newaz <newazredoy99@gmail.com>
2020-01-04 16:44:57 +0600

    name changed

--- lib/screen/questions/AskQuestionPage.dart
+++ lib/screen/questions/AskQuestionPage.dart
@@ -1,4 +1,4 @@
-import 'package:first_app/widgets/Search&Settings.dart';
+import 'package:hajjapp/widgets/Search&Settings.dart';
 import 'package:flutter/material.dart';
 
 class AskQuestionPage extends StatefulWidget {
