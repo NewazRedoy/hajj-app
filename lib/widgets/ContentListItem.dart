18562d38eb0b809a0206c5ddfb458d36e014f593
newaz <newazredoy99@gmail.com>
2020-05-10 21:40:15 +0600

    Copy

--- lib/widgets/ContentListItem.dart
+++ lib/widgets/ContentListItem.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hajjapp/screen/AboutApp.dart';
 import 'package:hajjapp/util/global.dart';
@@ -46,9 +47,8 @@ class _ContentListItemState extends State<ContentListItem> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
-                      decoration: BoxDecoration(color: Theme
-                          .of(context)
-                          .accentColor),
+                      decoration:
+                          BoxDecoration(color: Theme.of(context).accentColor),
                       width: 4,
                     ),
                     Container(
@@ -70,27 +70,44 @@ class _ContentListItemState extends State<ContentListItem> {
                     PopupMenuButton(
                       itemBuilder: (BuildContext context) {
                         return [
-                          PopupMenuItem(value: 1, child: ListTile(
-                            leading: Image.asset(
-                              "assets/images/More-Vert-Share.png", height: 20,
-                              width: 20,),
-                            title: Text("শেয়ার করুন"),
-                            onTap: () => share(context)
-//                                Navigator.push(
-//                              context,
-//                              MaterialPageRoute(
-//                                builder: (context) => AboutApp(),
-//                              ),
-//                            )
-                            ,)),
-                          PopupMenuItem(value: 1, child: ListTile(
-                            leading: Image.asset(
-                              "assets/images/More-Vert-Copy.png", height: 20,
-                              width: 20,), title: Text("কপি"),)),
-                          PopupMenuItem(value: 1, child: ListTile(
-                            leading: Image.asset(
-                              "assets/images/Menu-Bookmarks.png", height: 20,
-                              width: 20,), title: Text("বুকমার্ক"),)),
+                          PopupMenuItem(
+                              value: 1,
+                              child: ListTile(
+                                leading: Image.asset(
+                                  "assets/images/More-Vert-Share.png",
+                                  height: 20,
+                                  width: 20,
+                                ),
+                                title: Text("শেয়ার করুন"),
+                                onTap: () => share(context),
+                              )),
+                          PopupMenuItem(
+                              value: 2,
+                              child: ListTile(
+                                onTap: () {
+                                  Clipboard.setData(
+                                      ClipboardData(text: widget.content.text));
+                                  Scaffold.of(context).showSnackBar(SnackBar(
+                                    content: Text("Copied to Clipboard"),
+                                  ));
+                                },
+                                leading: Image.asset(
+                                  "assets/images/More-Vert-Copy.png",
+                                  height: 20,
+                                  width: 20,
+                                ),
+                                title: Text("কপি"),
+                              )),
+                          PopupMenuItem(
+                              value: 3,
+                              child: ListTile(
+                                leading: Image.asset(
+                                  "assets/images/Menu-Bookmarks.png",
+                                  height: 20,
+                                  width: 20,
+                                ),
+                                title: Text("বুকমার্ক"),
+                              )),
                         ];
                       },
                       // ignore: missing_return
@@ -116,53 +133,49 @@ class _ContentListItemState extends State<ContentListItem> {
               SizedBox(height: 6),
               widget.content.image?.isNotEmpty == true
                   ? Image.asset(
-                widget.content.image,
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width,
-              )
+                      widget.content.image,
+                      width: MediaQuery.of(context).size.width,
+                    )
                   : SizedBox(),
               widget.content.video_url?.isNotEmpty == true
                   ? InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => VideoPlayPage(widget.content),
-                      ));
-                },
-                child: Stack(
-                  children: <Widget>[
-                    CachedNetworkImage(
-                        imageUrl: YoutubePlayer.getThumbnail(
-                            videoId: "_4I7maBjaDk"),
-                        fit: BoxFit.cover,
-                        placeholder: (context, url) =>
-                            CircularProgressIndicator()),
-                    Positioned.fill(
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Icon(
-                          Icons.play_arrow,
-                          size: 64.0,
-                          color: Colors.white,
-                          semanticLabel: 'Text to announce in accessibility modes',
-                        ),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  VideoPlayPage(widget.content),
+                            ));
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          CachedNetworkImage(
+                              imageUrl: YoutubePlayer.getThumbnail(
+                                  videoId: "_4I7maBjaDk"),
+                              fit: BoxFit.cover,
+                              placeholder: (context, url) =>
+                                  CircularProgressIndicator()),
+                          Positioned.fill(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Icon(
+                                Icons.play_arrow,
+                                size: 64.0,
+                                color: Colors.white,
+                                semanticLabel:
+                                    'Text to announce in accessibility modes',
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-              )
+                    )
                   : SizedBox(),
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Text(
                   widget.content.text,
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .subhead,
+                  style: Theme.of(context).textTheme.subhead,
                 ),
               ),
               SizedBox(height: 6),
@@ -182,15 +195,12 @@ class _ContentListItemState extends State<ContentListItem> {
   }
 
   void share(BuildContext context) {
-
-    final String text =
-       widget.content.text
+    final String text = widget.content.text
 //        "Muslim Scholars & Companios is the ultimate collection of biographies "
 //        "having birth/death,narrator grade,family members and tags to inspire us to learn about them."
 //        "\n\nGet it now at Google Play Store:"
 //        '\nhttps://goo.gl/80yGtV'
         ;
-    Share.share(text,
-        subject: text);
+    Share.share(text, subject: text);
   }
 }
